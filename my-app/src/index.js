ffcdceb74adf10af08000b1b45ed4d11d705c826
Tyler Baguio <tylerb8@hawaii.edu>
2022-02-27 12:31:30 -1000

    Task 3

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button
+        className="square"
+        onClick={() => this.setState({ value: 'X' })}
+      >
+        {this.state.value}
       </button>
     );
   }
@@ -14,7 +24,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i}/>;
   }
 
   render() {
@@ -48,7 +58,7 @@ class Game extends React.Component {
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board/>
         </div>
         <div className="game-info">
           <div>{/* status */}</div>
@@ -62,7 +72,7 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(
-  <Game />,
+  <Game/>,
   document.getElementById('root')
 );
 
